f3336f44f250a11814e25080db502eaa33582f27
Evdp <enzo.vandepoele2@gmail.com>
2025-05-12 16:49:21 +0200

    1.1 Les composants de base

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { Button } from "@react-navigation/elements";
+import { Switch, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -7,9 +8,12 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        gap: "20px"
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button>Cliiiiiiiiiick</Button>
+      <Switch></Switch>
     </View>
   );
 }
